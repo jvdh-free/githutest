60e3c2f839e1e0a3ae7e498e6025001581f6b3b2
john Hoeven <jvdh_2@hotmail.com>
2018-12-20 07:41:00 -0800

    first change

--- github test/Program.cs
+++ github test/Program.cs
@@ -7,6 +7,7 @@ namespace github_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("this is a change");
         }
     }
 }
